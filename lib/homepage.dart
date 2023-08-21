06c0f22e0e1ea7193912be63eceee6ef79b15767
Abhiinav7 <abhinavcetl@gmail.com>
2023-08-21 22:24:38 +0530

    3rd commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,45 +9,44 @@ class MyHome extends StatefulWidget {
 class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return  Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar:AppBar(
+
         centerTitle: true,
         backgroundColor: Colors.black,
-        title: Text(
-          "Black",
-          style: TextStyle(
-            fontSize: 70,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold,
-            color: Colors.white
-
-          ),
+        title: const Text("Calculator",
+        style: TextStyle(
+            color: Colors.white,
+          fontSize: 50,
         ),
-      ),
-body: Container(
-  alignment: Alignment.center,
+        ),
+      ) ,
+      body: Container(
 
-  margin: EdgeInsets.all(10),
-  height: MediaQuery.of(context).size.height,
-  width: MediaQuery.of(context).size.width,
-  color: Colors.white,
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.20,
-        width: 150,
-        color: Colors.black,
-        //alignment: Alignment.center,
-        child: Center(
-          child: Text(
-            "black",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 50
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.green,
+        child: Container(
 
+          decoration: const BoxDecoration(
+            color: Colors.black,
+            shape: BoxShape.circle
+          ),
+          child:Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+                size: 50,
+              ),
             ),
           ),
         ),
-  )
-  ),
-);
+    ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 import 'homepage.dart';
-
-void main() {
+void main(){
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return  MaterialApp(
+      title: "demo app",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.lightBlue
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHome(),
+      home: const MyHome()
     );
   }
 }
+
