b1f571ee5451a00e9db7706e5d48b52adf4a1f6e
Abhiinav7 <abhinavcetl@gmail.com>
2023-08-22 17:49:52 +0530

    4th commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
 
@@ -9,44 +10,51 @@ class MyHome extends StatefulWidget {
 class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.lightBlue,
-      appBar:AppBar(
-
+      appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.black,
-        title: const Text("Calculator",
-        style: TextStyle(
+        backgroundColor: Colors.blueAccent,
+        title: const Text(
+          "Demo",
+          style: TextStyle(
             color: Colors.white,
-          fontSize: 50,
-        ),
-        ),
-      ) ,
-      body: Container(
-
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.green,
-        child: Container(
-
-          decoration: const BoxDecoration(
-            color: Colors.black,
-            shape: BoxShape.circle
+            fontSize: 50,
           ),
-          child:Center(
+        ),
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.80,
+          width: MediaQuery.of(context).size.width * 0.80,
+          margin: const EdgeInsets.only(bottom: 20,left: 20,right: 20,top: 20),
+          decoration: const BoxDecoration(
+              color: Colors.green,
+              borderRadius:
+                  BorderRadius.only(bottomRight: Radius.circular(30),bottomLeft: Radius.circular(30))),
+          child: Center(
             child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.red,
-              child: const Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 50,
+              width: 300,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                border: Border.all(width: 10, color: Colors.white),
+                borderRadius: const BorderRadius.all(Radius.circular(150)),
+                boxShadow: const [BoxShadow(blurRadius: 10, spreadRadius: 10)],
               ),
+              child: Icon(Icons.holiday_village)
             ),
           ),
         ),
-    ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){},
+        tooltip: "button",
+
+      ),
     );
   }
 }
